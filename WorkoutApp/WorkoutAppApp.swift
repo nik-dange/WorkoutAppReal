63ce792390f43e5ff323eb1ec9fe9361680ac8af
Nikhil <dange.nik@gmail.com>
2021-07-11 23:21:03 -0700

    Initial Commit

---
+++ WorkoutApp/WorkoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutAppApp.swift
+//  WorkoutApp
+//
+//  Created by Nikhil Dange on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
